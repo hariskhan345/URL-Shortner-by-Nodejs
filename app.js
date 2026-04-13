3ec25a32097c27260ecbed764085f59ef4497467
Haris Khattak <haris.ignipulse@gmail.com>
2026-04-13 15:50:53 +0500

    Setup for SSR using ejs

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const ConectMongoDb = require("./connections");
 const URLrouter = require("./routes/urlRoutes");
 const ShortURLModel = require("./models");
@@ -11,6 +12,14 @@ ConectMongoDb("mongodb://127.0.0.1:27017/ShortURL");
 app.use(express.json());
 
 app.use("/url", URLrouter);
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.get("/shorturl", async (req, res) => {
+  await ShortURLModel.find({});
+  return res.render("home");
+});
 
 app.get("/:shortId", async (req, res) => {
   const shortID = req.params.shortId;
